3af13ed555cf4b4f6886bdca1334787f5e65efdf
Isaac Peña <100379077+yzak6@users.noreply.github.com>
2024-03-03 20:23:03 -0700

    Update servwork.js

--- servwork.js
+++ servwork.js
@@ -12,51 +12,31 @@ const CACHE_NAME = 'v1_cache_Mipagina',
     'img/yogif.gif',
   ]
 
-//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
-self.addEventListener('install', e => {
-  e.waitUntil(
+self.addEventListener('install', event => {
+    event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache)
-          .then(() => self.skipWaiting())
-      })
-      .catch(err => console.log('Falló registro de cache', err))
-  )
-})
+        .then(cache => cache.addAll(urlsToCache))
+    );
+});
 
-//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
-self.addEventListener('activate', e => {
-  const cacheWhitelist = [CACHE_NAME]
+self.addEventListener('fetch', event => {
+    event.respondWith(
+    caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+});
 
-  e.waitUntil(
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+
+    event.waitUntil(
     caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            //Eliminamos lo que ya no se necesita en cache
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName)
+        .then(cacheNames => Promise.all(
+        cacheNames.map(cacheName => {
+            if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
             }
-          })
-        )
-      })
-      // Le indica al SW activar el cache actual
-      .then(() => self.clients.claim())
-  )
-})
-
-//cuando el navegador recupera una url
-self.addEventListener('fetch', e => {
-  //Responder ya sea con el objeto en caché o continuar y buscar la url real
-  e.respondWith(
-    caches.match(e.request)
-      .then(res => {
-        if (res) {
-          //recuperar del cache
-          return res
-        }
-        //recuperar de la petición a la url
-        return fetch(e.request)
-      })
-  )
-})
+        })
+        ))
+    );
+});
